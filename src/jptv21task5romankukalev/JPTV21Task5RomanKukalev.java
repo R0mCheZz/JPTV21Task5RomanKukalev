dbefc798c156b1f4c73c2c0250f32216d4353294
pupil <pupil@ivkhk.ee>
2022-09-28 14:55:23 +0300

    Done

--- src/jptv21task5romankukalev/JPTV21Task5RomanKukalev.java
+++ src/jptv21task5romankukalev/JPTV21Task5RomanKukalev.java
@@ -5,6 +5,14 @@
  */
 package jptv21task5romankukalev;
 import java.util.Arrays;
+import java.util.Random;
+/**
+ *
+ * @author pupil
+ */
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  *
  * @author pupil
@@ -14,25 +22,48 @@ public class JPTV21Task5RomanKukalev {
     /**
      * @param args the command line arguments
      */
+    static int getIntFromRange(int min, int max){
+        Random rand = new Random();
+        return rand.nextInt(max-min+1)+min;
+    }
     public static void main(String[] args) {
-        
-        int[][] arr = new int [5][];
-        arr[0] = new int [3];
-        arr[1] = new int [5];
-        arr[2] = new int [7];
-        arr[3] = new int [8];
+        // TODO code application logic here
+        int min = 100;
+        int max = 0;
+        int[][] arr = new int[5][];
+        arr[0] = new int[3];
+        arr[1] = new int[5];
+        arr[2] = new int[7];
+        arr[3] = new int[8];
         arr[4] = new int [9];
-        for (int[] arr1 : arr) {
-            ;
-        }
-{
-            int i = 0;
-            for (int j = 0; j < arr[i].length; j++){
-                arr[i][j] = ((int)(Math.random() *50 ));
-                System.out.print(" " + arr[i][j]+ " ");
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                arr[i][j] = getIntFromRange(50,100);
             }
-            System.out.println();
+            System.out.println(Arrays.toString(arr[i]));
+        }
+        System.out.println("=====================================");
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                for(int n = 0; n < arr.length; n++){
+                    for(int m = 0; m < arr[n].length; m++){
+                        if(arr[i][j]>arr[n][m]){
+                            arr[i][j] = arr[i][j]+arr[n][m];
+                            arr[n][m] = arr[i][j]-arr[n][m];
+                            arr[i][j] = arr[i][j]-arr[n][m];
+                        }
+                    }
+                }           
+            }   
+            
+          }
+          for(int i = 0; i < arr.length; i++){
+                for(int j = 0; j < arr[i].length; j++){
+                
+                
+                    System.out.printf("%4d", arr[i][j]);
+                }
+                System.out.println();
+          }
     }
-    }
-    
 }
